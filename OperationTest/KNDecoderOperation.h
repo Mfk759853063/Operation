dd5866b624ea441532e90adee8cc0bc9c3525d5c
vbn <zhoucan123@vip.qq.com>
2016-07-26 17:57:25 +0800

    first commit

---
+++ OperationTest/KNDecoderOperation.h
@@ -0,0 +1,13 @@
+//
+//  KNDecoderOperation.h
+//  OperationTest
+//
+//  Created by vbn on 16/7/26.
+//  Copyright © 2016年 vbn. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface KNDecoderOperation : NSOperation
+
+@end
---
+++ OperationTest/KNDecoderOperation.m
@@ -0,0 +1,13 @@
+//
+//  KNDecoderOperation.m
+//  OperationTest
+//
+//  Created by vbn on 16/7/26.
+//  Copyright © 2016年 vbn. All rights reserved.
+//
+
+#import "KNDecoderOperation.h"
+
+@implementation KNDecoderOperation
+
+@end
